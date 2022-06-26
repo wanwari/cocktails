db3b32804d4235d5770b61ff8059c6eb19a81322
Wiesa Anwari <wiesa@wanwari.ca>
2022-06-26 14:37:08 -0400

    [config]: install & setup Auth0

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 import "./index.css";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
 
+const domain: any = process.env.AUTH0_DOMAIN;
+const clientId: any = process.env.AUTH0_CLIENT_ID;
+
 root.render(
 	<React.StrictMode>
-		<App />
+		<Auth0Provider
+			domain={domain}
+			clientId={clientId}
+			redirectUri={window.location.origin}
+		>
+			<App />
+		</Auth0Provider>
 	</React.StrictMode>
 );
