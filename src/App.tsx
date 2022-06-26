30b771289db41e10c209cc7a7d1ff3e833a2194c
Wiesa Anwari <wiesa@wanwari.ca>
2022-06-26 11:23:50 -0400

    [config]: install & setup react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
-	return <div>Cocktails</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="*" element={<NotFound />} />
+				<Route path="/" element={<Main />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
